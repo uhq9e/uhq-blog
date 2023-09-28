15605a0fbde3f8c573a006dc872ceba03d1aee01
iitenkie <846708308gm@gmail.com>
2023-09-28 21:17:23 +0800

    upd

--- src/utils/generateOgImages.tsx
+++ src/utils/generateOgImages.tsx
@@ -31,13 +31,13 @@ const options: SatoriOptions = {
   embedFont: true,
   fonts: [
     {
-      name: "IBM Plex Mono",
+      name: "MiSans",
       data: fontRegular,
       weight: 400,
       style: "normal",
     },
     {
-      name: "IBM Plex Mono",
+      name: "MiSans",
       data: fontBold,
       weight: 600,
       style: "normal",
